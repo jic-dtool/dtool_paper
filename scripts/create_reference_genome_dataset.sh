5caa8872df8125b403c3fc5a01c191fbf00059cb
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-09-24 16:29:48 +0100

    Improve the create ref genome dataset bash script

--- scripts/create_reference_genome_dataset.sh
+++ scripts/create_reference_genome_dataset.sh
@@ -12,17 +12,26 @@ ACCESSION_ID=$1
 # Save working directory.
 CUR_DIR=`pwd`
 
+# Specify the URLs to use.
+BASE_URL="https://www.ebi.ac.uk/ena/data/view/$ACCESSION_ID"
+FASTA_URL="$BASE_URL&display=fasta"
+TEXT_URL="$BASE_URL&display=text&header=true"
+
+# Get the organism from the EMBL file format. 
+ORGANISM=`curl $TEXT_URL | grep '^OS' | sed s'/^OS   //'`
+
+
 # Create the proto dataset.
-DS_NAME=$ACCESSION_ID-ref-genome
+DS_NAME=`echo $ORGANISM | cut -d" " -f1 -f2 | sed s'/ /-/'`-ref-genome
+echo $DS_NAME
 DS_URI=`dtool create -q $DS_NAME`
 
 # Move into the data directory.
 cd $DS_NAME/data
 
 # Download the genome from the ENA.
-URL="https://www.ebi.ac.uk/ena/data/view/$ACCESSION_ID&display=fasta"
 FNAME=$ACCESSION_ID.fasta
-curl $URL > $FNAME
+curl $FASTA_URL > $FNAME
 
 # Build the Bowtie2 indices.
 INDEX_BUILDER=bowtie2-build
@@ -35,8 +44,9 @@ cd $CUR_DIR
 # Add descriptive metadata.
 README=$DS_NAME/README.yml
 echo "description: $ACCESSION_ID genome with Bowtie2 indices" > $README
+echo "organism: $ORGANISM" >> $README
 echo "accession_id: $ACCESSION_ID" >> $README
-echo "link: $URL" >> $README
+echo "link: $BASE_URL" >> $README
 echo "index_builder: `$INDEX_BUILDER --version | head -1`" >> $README
 echo "index_build_cmd: $INDEX_BUILD_CMD" >> $README
 
