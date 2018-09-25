0afaf0ac6ee625acb5278f470586a937b0b59ee6
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-09-25 22:00:43 +0100

    Add more comments to the minify.sh script

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -8,20 +8,30 @@ INPUT_URI=$1
 OUTPUT_BASE_URI=$2
 NUM_LINES=4000
 
-echo $INPUT_URI
-
+# Create a name for the output dataset based on the input dataset.
 OUTPUT_NAME=`dtool name $INPUT_URI`-minified
-echo $OUTPUT_NAME
-echo $OUTPUT_BASE_URI
 
+# Create an open proto dataset.
 OUTPUT_URI=`dtool create -q $OUTPUT_NAME $OUTPUT_BASE_URI`
 
+# Process all the items in the input dataset and
+# add the results to the output dataset.
 for ITEM_ID in `dtool identifiers $INPUT_URI`; do
+
+  # Fetch the item from the dataset and get an absolute path
+  # from where its content can be accessed.
   ITEM_ABSPATH=`dtool item fetch $INPUT_URI $ITEM_ID`
-  RELPATH=`dtool item relpath $INPUT_URI $ITEM_ID`
+
+  # Write the minified version of the item to a temporary file.
   TMP_MINIFIED=$(mktemp /tmp/minfied.XXXXXX)
   gunzip -c $ITEM_ABSPATH | head -n $NUM_LINES | gzip > $TMP_MINIFIED
+
+  # Add the temporary file to the output dataset giving it the relpath
+  # of the item from the input dataset.
+  RELPATH=`dtool item relpath $INPUT_URI $ITEM_ID`
   dtool add item $TMP_MINIFIED $OUTPUT_URI $RELPATH
+
+  # Cleanup.
   rm $TMP_MINIFIED
 done
 
@@ -33,8 +43,10 @@ echo "  from_UUID: `dtool uuid $INPUT_URI`" >> $TMP_README
 echo "  from_URI: $INPUT_URI" >> $TMP_README
 echo "  content: first $NUM_LINES per item" >> $TMP_README
 
-# Add the descriptive metadata to the output dataset
+# Add the descriptive metadata to the output dataset.
 dtool readme write $OUTPUT_URI $TMP_README
+
+# Cleanup.
 rm $TMP_README
 
 # Finalise the output dataset.
